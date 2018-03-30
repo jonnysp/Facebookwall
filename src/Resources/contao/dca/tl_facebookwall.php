f3a9a0c9473376f5d5b333fa1519ff3628db2420
jonny <jonnyspitzner@googlemail.com>
2018-03-30 07:47:45 +0200

    add info

--- src/Resources/contao/dca/tl_facebookwall.php
+++ src/Resources/contao/dca/tl_facebookwall.php
@@ -70,7 +70,13 @@ $GLOBALS['TL_DCA']['tl_facebookwall'] = array
 				'href'                => 'act=delete',
 				'icon'                => 'delete.svg',
 				'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
-			)
+			),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_facebookwall']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.svg'
+            )
 		)
 	),
 
